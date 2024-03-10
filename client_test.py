00be76db363c1bf56f39bc7ed6fb248bd82ffc8c
Jonathan D Vann <jonathan.vann@uconn.edu>
2024-03-10 12:22:08 -0400

    fixed getDataPoint and getRatio; added unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       # getDataPoint(quote) returns: stock, bid_price, ask_price, price(bid_price+ask_price/2)
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +18,29 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    prices_list = [
+       {'ABC': 111.915, 'DEF': 192.035},
+       {'ABC': 319.111, 'DEF': 143.209}
+    ]
+
+    for prices in prices_list:
+       self.assertEqual(getRatio(prices['ABC'], prices['DEF']), (prices['ABC']/prices['DEF']))
+
+
+
+  def test_getRatio_divideByZero(self):
+    prices_list = [
+       {'ABC': 111.915, 'DEF': 0},
+       {'ABC': 0, 'DEF': 0}
+    ]
+    for prices in prices_list:
+       self.assertIsNone(getRatio(prices['ABC'], prices['DEF']))
+       
 
 
 
